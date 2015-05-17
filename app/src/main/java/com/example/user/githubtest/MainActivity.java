7fdba4d1f11952369f19b5a8197973bdba7d5acc
tadcc15 <aings@tccn.edu.tw>
2015-05-17 16:20:36 +0800

    test update

--- app/src/main/java/com/example/user/githubtest/MainActivity.java
+++ app/src/main/java/com/example/user/githubtest/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //update test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
